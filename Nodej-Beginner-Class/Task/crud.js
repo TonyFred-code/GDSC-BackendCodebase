556841e2a6d77f21aed1f02ae9337124b50199d9
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-03-31 19:10:59 +0100

    feat; solve CRUD operations using fs module

--- Nodej-Beginner-Class/Task/crud.js
+++ Nodej-Beginner-Class/Task/crud.js
@@ -1,9 +1,58 @@
 const fs = require('fs');
 
 // create a file
+// const content = 'Creating a file and putting in text';
+// fs.writeFile('create.txt', content, function(err) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log('Created file and wrote in')
+// })
 
 // read a file
+// fs.readFile('./Bin.txt', 'utf-8', function(err, data) {
+//   if (err) throw err;
+
+//   console.log('data read successfully');
+//   console.log(data)
+// });
 
 // update a file
+// const content = 'Creating a file and putting in text';
+// fs.writeFile('create.txt', content, function(err) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log('Created file and wrote in')
+// }); // creating a file;
+
+// fs.appendFile('./create.txt', 'some new txt', function(err) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log('update successful')
+// })
 
 // delete a file
+
+// const content = 'Creating a file and putting in text';
+
+// fs.writeFile('create.txt', content, function (err) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log('Created file and wrote in');
+// }); // create file
+
+// fs.unlink('./create.txt', (err) => {
+//   if (err) {
+//     console.error('encountered error while trying to delete file');
+//     return -1;
+//   }
+
+//   console.info('successfully deleted file');
+// });
